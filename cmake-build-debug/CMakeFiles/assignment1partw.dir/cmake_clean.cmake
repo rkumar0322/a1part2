890d0a3605fc7cd908ca88a8bdebdf482404330c
Rahul Kumar <rk32298@gmail.com>
2020-01-24 19:59:23 -0500

    commit

---
+++ cmake-build-debug/CMakeFiles/assignment1partw.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment1partw.dir/main.cpp.o"
+  "assignment1partw"
+  "assignment1partw.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment1partw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
