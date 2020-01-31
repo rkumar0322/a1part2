5a03ee90b31c028ce69030cc2303830bf6d18076
Xinhan-Yan <yan.xi@husky.neu.edu>
2020-01-31 14:46:22 -0500

    remove implementations & add 4 supported classes

--- part2/array.h
+++ part2/array.h
@@ -4,6 +4,7 @@
 
 #include <cstddef>
 #include "object.h"
+#include "string.h"
 
 #ifndef ASSIGNMENT1PARTW_ARRAY_H
 #define ASSIGNMENT1PARTW_ARRAY_H
@@ -14,42 +15,33 @@
 class Array: public Object {
 public:
 
-    Object* array;
-    size_t size;
-    size_t MAX_SIZE;
-
     //Constructor:
-    //initializing an empty array with input size
+    //initializing an array with input size
     Array(size_t limit) {
-        Object[] l = new Object[limit];
-        array = *l;
-        size = 0;
-        MAX_SIZE = limit;
     }
     
+    //destructor
     ~Array() {
-        delete[] l;
-        delete array;
     }
     
 
     //assign the element at index with input object
-    void assign(Object element, size_t index) {
+    void assign(Object* element, size_t index) {
 
     }
     
     //returns the element at index 
-    Object get(size_t index) {
+    Object* get(size_t index) {
         
     }
     
     //appends e to the end of the array 
-    void push_back(Object o) {
+    void push_back(Object* o) {
         
     }
     
     //inserts o at index
-    void add(Object o, size_t index) {
+    void add(Object* o, size_t index) {
         
     }
     
@@ -59,7 +51,7 @@ public:
     }
     
     //compares o with this array for equality 
-    bool equals(Object o) {
+    bool equals(Object* o) {
         
     }
 
@@ -68,4 +60,200 @@ public:
 
     }
 
+};
+
+class StrArray: public Object {
+public:
+    
+    //Constructor:
+    //initializing a string array with input size
+    StrArray(size_t limit) {   
+    }
+    
+    //destructor 
+    ~StrArray() {
+    }
+
+   
+    //assign the element at index with input object
+    void assign(String* s, size_t index) {
+
+    }
+    
+    //returns the string at index 
+    String* get(size_t index) {
+        
+    }
+    
+    //appends s to the end of the array 
+    void push_back(String* s) {
+        
+    }
+    
+    //inserts s at index
+    void add(String* s, size_t index) {
+        
+    }
+    
+    //removes all elements in the array 
+    void clear() {
+        
+    }
+    
+    //compares o with this array for equality 
+    bool equals(Object* o) {
+        
+    }
+
+    //returns the number of elements in the array
+    size_t length() {
+
+    }
+
+};
+
+class FloatArray: public Object {
+public:
+
+    //Constructor:
+    //initializing a float array with input size
+    FloatArray(size_t limit) {
+    }
+    
+    ~FloatArray() {
+    }
+   
+   
+    //assign the element at index with input float
+    void assign(float element, size_t index) {
+
+    }
+    
+    //returns the element at index 
+    float get(size_t index) {
+        
+    }
+    
+    //appends f to the end of the array 
+    void push_back(float f) {
+        
+    }
+    
+    //inserts f at index
+    void add(float f, size_t index) {
+        
+    }
+    
+    //removes all elements in the array 
+    void clear() {
+        
+    }
+    
+    //compares o with this array for equality 
+    bool equals(Object* o) {
+        
+    }
+
+    //returns the number of elements in the array
+    size_t length() {
+
+    }
+
+};
+
+class IntArray: public Object {
+public:
+
+    //Constructor:
+    //initializing an int array with input size
+    IntArray(size_t limit) {
+    }
+    
+    //destructor 
+    ~IntArray() {
+    }
+  
+    //assign the element at index with input int
+    void assign(int i, size_t index) {
+
+    }
+    
+    //returns the int at index 
+    int get(size_t index) {
+        
+    }
+    
+    //appends i to the end of the array 
+    void push_back(int i) {
+        
+    }
+    
+    //inserts i at index
+    void add(int i, size_t index) {
+        
+    }
+    
+    //removes all elements in the array 
+    void clear() {
+        
+    }
+    
+    //compares o with this array for equality 
+    bool equals(Object* o) {
+        
+    }
+
+    //returns the number of elements in the array
+    size_t length() {
+
+    }
+
+};
+
+class BoolArray: public Object {
+public:
+
+    //Constructor:
+    //initializing a bool array with input size
+    BoolArray(size_t limit) {
+    }
+   
+    //destructor
+    ~BoolArray() {
+    }
+  
+    //assign the boolean at index with input object
+    void assign(bool b, size_t index) {
+
+    }
+    
+    //returns the bool at index 
+    bool get(size_t index) {
+        
+    }
+    
+    //appends b to the end of the array 
+    void push_back(bool b) {
+        
+    }
+    
+    //inserts b at index
+    void add(bool b, size_t index) {
+        
+    }
+    
+    //removes all elements in the array 
+    void clear() {
+        
+    }
+    
+    //compares o with this array for equality 
+    bool equals(Object* o) {
+        
+    }
+
+    //returns the number of elements in the array
+    size_t length() {
+
+    }
 };
