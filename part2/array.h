39bf57b10793ef27f8092a77d86c6d4d65870202
kevlee1 <lee.kev@husky.neu.edu>
2020-01-31 15:24:21 -0500

    Update array.h

--- part2/array.h
+++ part2/array.h
@@ -62,198 +62,53 @@ public:
 
 };
 
-class StrArray: public Object {
+class Float: public Object {
 public:
-    
+
     //Constructor:
-    //initializing an empty string array
-    StrArray() {   
+    //initializing an empty float
+    Float() {
+    }
+    
+    ~Float() {
+    }
+    //compares o with this float for equality 
+    bool equals(Object* o) {
+        
+    }
+};
+
+class Integer: public Object {
+public:
+
+    //Constructor:
+    //initializing an empty Integer
+    Integer() {
     }
     
     //destructor 
-    ~StrArray() {
+    ~Integer() {
     }
-
-   
-    //assign the element at index with input object
-    void assign(String* s, size_t index) {
-
-    }
-    
-    //returns the string at index 
-    String* get(size_t index) {
-        
-    }
-    
-    //appends s to the end of the array 
-    void push_back(String* s) {
-        
-    }
-    
-    //inserts s at index
-    void add(String* s, size_t index) {
-        
-    }
-    
-    //removes all elements in the array 
-    void clear() {
-        
-    }
-    
-    //compares o with this array for equality 
+    //compares o with this Integer for equality 
     bool equals(Object* o) {
         
     }
-
-    //returns the number of elements in the array
-    size_t length() {
-
-    }
-
 };
 
-class FloatArray: public Object {
+class Bool: public Object {
 public:
 
     //Constructor:
-    //initializing an empty float array
-    FloatArray() {
-    }
-    
-    ~FloatArray() {
-    }
-   
-   
-    //assign the element at index with input float
-    void assign(float element, size_t index) {
-
-    }
-    
-    //returns the element at index 
-    float get(size_t index) {
-        
-    }
-    
-    //appends f to the end of the array 
-    void push_back(float f) {
-        
-    }
-    
-    //inserts f at index
-    void add(float f, size_t index) {
-        
-    }
-    
-    //removes all elements in the array 
-    void clear() {
-        
-    }
-    
-    //compares o with this array for equality 
-    bool equals(Object* o) {
-        
-    }
-
-    //returns the number of elements in the array
-    size_t length() {
-
-    }
-
-};
-
-class IntArray: public Object {
-public:
-
-    //Constructor:
-    //initializing an empty int array
-    IntArray() {
-    }
-    
-    //destructor 
-    ~IntArray() {
-    }
-  
-    //assign the element at index with input int
-    void assign(int i, size_t index) {
-
-    }
-    
-    //returns the int at index 
-    int get(size_t index) {
-        
-    }
-    
-    //appends i to the end of the array 
-    void push_back(int i) {
-        
-    }
-    
-    //inserts i at index
-    void add(int i, size_t index) {
-        
-    }
-    
-    //removes all elements in the array 
-    void clear() {
-        
-    }
-    
-    //compares o with this array for equality 
-    bool equals(Object* o) {
-        
-    }
-
-    //returns the number of elements in the array
-    size_t length() {
-
-    }
-
-};
-
-class BoolArray: public Object {
-public:
-
-    //Constructor:
-    //initializing an empty bool array 
-    BoolArray() {
+    //initializing an empty bool 
+    Bool() {
     }
    
     //destructor
-    ~BoolArray() {
+    ~Bool() {
     }
-  
-    //assign the boolean at index with input object
-    void assign(bool b, size_t index) {
-
-    }
-    
-    //returns the bool at index 
-    bool get(size_t index) {
-        
-    }
-    
-    //appends b to the end of the array 
-    void push_back(bool b) {
-        
-    }
-    
-    //inserts b at index
-    void add(bool b, size_t index) {
-        
-    }
-    
-    //removes all elements in the array 
-    void clear() {
-        
-    }
-    
-    //compares o with this array for equality 
+ 
+    //compares o with this bool for equality 
     bool equals(Object* o) {
         
-    }
-
-    //returns the number of elements in the array
-    size_t length() {
-
     }
 };
